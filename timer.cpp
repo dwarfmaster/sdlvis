31b4e787752620dedbe73da80a75604537ccd46a
lucas8 <luc.linux@mailoo.org>
2012-08-11 13:59:21 +0200

    Fin du timer.

--- timer.cpp
+++ timer.cpp
@@ -1,3 +1,40 @@
 #include "timer.hpp"
 
+Timer::Timer(unsigned int time, boost::function<void ()> cb)
+	: m_callback(cb), m_time(time), m_lastTime(SDL_GetTicks()), m_timeStay(m_time), m_pause(true)
+{
+}
+
+void Timer::play()
+{
+	if( !m_pause )
+		return;
+
+	m_pause = false;
+	m_lastTime = SDL_GetTicks();
+}
+
+void Timer::pause()
+{
+	update();
+	m_pause = true;
+}
+
+void Timer::update()
+{
+	if( m_pause )
+		return;
+
+	Uint32 m_timeLapsed = SDL_GetTicks() - m_lastTime;
+	while(m_timeLapsed > m_timeStay)
+	{
+		if( m_callback )
+			m_callback();
+		m_timeLapsed -= m_timeStay;
+		m_timeStay = m_time;
+	}
+
+	m_timeStay -= m_timeLapsed;
+	m_lastTime = SDL_GetTicks();
+}
 
--- timer.hpp
+++ timer.hpp
@@ -2,11 +2,26 @@
 #define DEF_TIMER
 
 #include <boost/function.hpp>
+#include <SDL/SDL.h>
 
 class Timer
 {
 	public:
+		Timer(unsigned int time, boost::function<void ()> cb);
+		void play();
+		void pause();
+		void update();
+
 	private:
+		boost::function<void ()> m_callback;
+		Uint32 m_time; // temps entre deux appels
+		Uint32 m_lastTime;
+		Uint32 m_timeStay; // temps restant
+		bool m_pause;
+
+		Timer();
+		Timer(const Timer&);
+		Timer& operator=(const Timer&);
 };
 
 #endif
