a9cbecf685a13865d0b619b51675301c9e6c230c
lucas8 <luc.linux@mailoo.org>
2012-08-11 13:23:36 +0200

    Ajout de la gestion des exceptions et chargement de la SDL.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <exception>
 
 #include "config.hpp"
 
 int main(int argc, char *argv[])
 {
-	ConfigLoader conf(argc, argv);
-	conf.load();
+	try
+	{
+		if(SDL_Init(SDL_INIT_VIDEO) < 0)
+			throw std::string("Erreur à l'initialsiation de la lib SDL.");
+
+		ConfigLoader conf(argc, argv);
+		conf.load();
+	}
+	catch(const std::string& str)
+	{
+		std::cout << "Error thrown by program : " << str << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Error thrown by standart library : " << e.what() << std::endl;
+	}
+	catch(...)
+	{
+		std::cout << "Unknown error." << std::endl;
+	}
+
+	SDL_Quit();
 
 	return 0;
 }
