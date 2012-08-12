dbaf748ad4f37a3da84df08cd70ea8ab37c2ed75
lucas8 <luc.linux@mailoo.org>
2012-08-12 17:00:38 +0200

    Fin de la classe print.

---
+++ print.cpp
@@ -0,0 +1,416 @@
+#include "print.hpp"
+
+
+	Printer::Printer(config conf)
+: m_config(conf)
+{
+	if( verbose() )
+		std::cout << "\n" << std::endl; // Espace avant les messages de la suite
+
+	if( !mute() )
+		std::cout << "Chargements." << std::endl;
+
+	loadWindow();
+	loadErr();
+	if( m_config.text )
+		loadFont(); // Charge la police
+	load(); // Charge les images
+
+	if( m_config.diap )
+		loadDiap();
+
+	loadFirst();
+}
+
+void Printer::run()
+{
+	if( !mute() )
+		std::cout << "Launching program." << std::endl;
+
+	sdl::Event event;
+	loadKeys(&event);
+
+	SDL_Rect pos;
+
+	if( m_config.diap )
+		m_diapTimer->play();
+
+	while( !event.quit() )
+	{
+		event.update();
+		if( m_config.diap )
+			m_diapTimer->update();
+
+		SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
+
+		pos.x = ecran->w / 2 - m_picts[m_act].surf->w / 2;
+		pos.y = ecran->h / 2 - m_picts[m_act].surf->h / 2;
+		SDL_BlitSurface(m_picts[m_act].surf, NULL, ecran, &pos);
+
+		if( m_config.text )
+		{
+			pos.x = ecran->w /2 - m_picts[m_act].txt->w / 2;
+			pos.y = ecran->h - m_picts[m_act].txt->h;
+			SDL_BlitSurface(m_picts[m_act].txt, NULL, ecran, &pos);
+		}
+
+		SDL_Flip(ecran);
+
+		SDL_Delay( 1000/30 );
+	}
+}
+
+Printer::~Printer()
+{
+	SDL_FreeSurface(m_err);
+
+	if( m_config.text )
+	{
+		TTF_CloseFont( m_font );
+		if( !m_config.ptext )
+			SDL_FreeSurface( m_picts[m_act].txt );
+		else
+		{
+			for(size_t i=0; i < m_picts.size(); ++i)
+				SDL_FreeSurface( m_picts[i].txt );
+		}
+	}
+
+	if( !m_config.begin )
+	{
+			for(size_t i=0; i < m_picts.size(); ++i)
+				SDL_FreeSurface( m_picts[i].surf );
+	}
+	else
+		SDL_FreeSurface( m_picts[m_act].surf );
+}
+
+void Printer::load()
+{
+	if( verbose() )
+		std::cout << "Preloading pictures." << std::endl;
+
+	m_picts.resize( m_config.paths.size() );
+
+	for(size_t i=0; i < m_picts.size(); ++i)
+	{
+		m_picts[i].path = m_config.paths[i];
+
+		if( highverb() )
+			std::cout << "\t[" << i+1 << "/" << m_picts.size() << "]> Preloading of " << m_picts[i].path.string() << std::endl;
+
+		if( m_config.prep ) // Si on doit préparer les images au démarrage
+		{
+			if( prepare( &m_picts[i] ) )
+			{
+				if( m_config.begin ) // Si on doit charger les images au démarrage
+					load( &m_picts[i] );
+			}
+		}
+
+		if( m_config.ptext ) // Si on doit charger le texte au démarrage
+			loadTxt( &m_picts[i], m_config.textaa, i + 1 );
+	}
+}
+
+void Printer::loadTxt(Picture* pict, bool aa, size_t number)
+{
+	SDL_Color fg;
+	fg.r = fg.g = fg.b = 255;
+
+	std::string name("");
+
+	if( m_config.number )
+	{
+		name += "[";
+		name += sdl::itoa(number); // TODO
+		name += "/";
+		name += sdl::itoa( m_picts.size() );
+		name += "]> ";
+	}
+
+	name += pict->path.filename().string();
+
+	if( highverb() )
+		std::cout << "\t-> Loading text surface for " << name << std::endl;
+	SDL_Surface* tmp = NULL;
+
+	if( aa )
+		tmp = TTF_RenderText_Blended(m_font, name.c_str(), fg);
+	else
+		tmp = TTF_RenderText_Solid(m_font, name.c_str(), fg);
+
+	if( tmp == NULL ) // Si on n'arrive pas à charger le texte, on le désactive
+	{
+		if( !mute() )
+			std::cout << "Impossible de charger le texte " << name <<".\n\t-> Désactivation du texte." << std::endl;
+
+		m_config.text = false;
+		m_config.ptext = false;
+	}
+
+	pict->txt = tmp;
+}
+
+void Printer::loadFont()
+{
+	if( verbose() )
+		std::cout << "Loading font." << std::endl;
+
+	m_font = TTF_OpenFont("font.ttf", 15);
+
+	if( m_font == NULL )
+	{
+		if( !mute() )
+			std::cout << "Erreur au chargement de la police.\n\t-> Désactivation du texte." << std::endl;
+
+		m_config.text = false;
+		m_config.ptext = false;
+	}
+}
+
+bool Printer::prepare(Picture* pict, bool force_guard)
+{
+	if( pict->prepared )
+		return true;
+
+	pict->surf = IMG_Load( pict->path.string().c_str() );
+	if( pict->surf == NULL )
+	{
+		if( !mute() )
+			std::cout << "Erreur au chargement de l'image " << pict->path.string() << std::endl;
+		return false;
+	}
+
+	if( pict->surf->w > ecran->w
+			|| pict->surf->h > ecran->h ) // S'il est plus grand
+	{
+		pict->bigger = true;
+		if( !m_config.real )
+		{
+			pict->redx = double(ecran->w) / double(pict->surf->w);
+			pict->redy = double(ecran->h) / double(pict->surf->h);
+		}
+	}
+	else if( m_config.redim ) // S'il est plus petit
+	{
+		pict->bigger = false;
+		pict->redx = double(ecran->w) / double(pict->surf->w);
+		pict->redy = double(ecran->h) / double(pict->surf->h);
+	}
+
+	if( !m_config.deform )
+	{
+		pict->redx = (pict->redx > pict->redy) ? pict->redy : pict->redx;
+		pict->redy = pict->redx;
+	}
+
+	if( !m_config.begin && !force_guard )
+	{
+		SDL_FreeSurface( pict->surf );
+		pict->surf = NULL;
+	}
+
+	pict->prepared = true;
+	return true;
+}
+
+void Printer::load(Picture* pict, bool aa)
+{
+	SDL_Surface* tmp = NULL;
+	if( pict->surf == NULL)
+		tmp = IMG_Load( pict->path.string().c_str() );
+	else
+		tmp = pict->surf;
+
+	if(tmp == NULL)
+	{
+		if( !mute() )
+			std::cout << "Erreur au chargement de l'image " << pict->path.string() << std::endl;
+		pict->surf = m_err;
+	}
+	else
+	{
+		pict->surf = zoomSurface(tmp, pict->redx, pict->redy, aa);
+		pict->aa = aa;
+
+		if(pict->surf == NULL)
+		{
+			if( !mute() )
+				std::cout << "Erreur au redimentionnement de l'image " << pict->path.string() << std::endl;
+			pict->surf = m_err;
+		}
+		else
+			SDL_FreeSurface(tmp);
+	}
+}
+
+void Printer::toggleAA()
+{
+	SDL_FreeSurface( m_picts[m_act].surf );
+	m_picts[m_act].surf = NULL;
+	load( &m_picts[m_act], !m_picts[m_act].aa );
+}
+
+inline bool Printer::mute()
+{
+	return m_config.verb == config::MUTE;
+}
+
+inline bool Printer::verbose()
+{
+	return m_config.verb >= config::VERBOSE;
+}
+
+inline bool Printer::highverb()
+{
+	return m_config.verb >= config::HIGH;
+}
+
+void Printer::next(bool ret)
+{
+	size_t last = m_act;
+	++m_act;
+	if( m_act >= m_picts.size() )
+	{
+		if( ret )
+			m_act = 0;
+		else
+		{
+			--m_act;
+			return;
+		}
+	}
+
+	update(last);
+}
+
+void Printer::prev(bool ret)
+{
+	size_t last = m_act;
+	if( m_act == 0 )
+	{
+		if( ret )
+			m_act = m_picts.size() - 1;
+		else
+			return;
+	}
+	else
+		--m_act;
+
+	update(last);
+}
+
+void Printer::first()
+{
+	size_t last = m_act;
+	m_act = 0;
+
+	update(last);
+}
+
+void Printer::last()
+{
+	size_t last = m_act;
+	m_act = m_picts.size() - 1;
+
+	update(last);
+}
+
+void Printer::update(size_t last)
+{
+	if( !m_config.prep )
+		prepare( &m_picts[m_act], true );
+
+	if( !m_config.begin )
+	{
+		SDL_FreeSurface( m_picts[last].surf );
+		m_picts[last].surf = NULL;
+		load( &m_picts[m_act] );
+	}
+
+	if( m_config.text && !m_config.ptext )
+	{
+		SDL_FreeSurface( m_picts[last].txt );
+		m_picts[last].txt = NULL;
+		loadTxt( &m_picts[m_act], m_config.textaa, m_act + 1 );
+	}
+
+	std::string caption("sdlvis - ");
+	caption += m_picts[m_act].path.filename().string();
+	SDL_WM_SetCaption(caption.c_str(), NULL);
+}
+
+void Printer::loadWindow()
+{
+	if( !mute() )
+		std::cout << "Chargement de la fenêtre." << std::endl;
+
+	Uint32 flags = SDL_HWSURFACE | SDL_DOUBLEBUF;
+	if( m_config.fullscreen )
+		flags |= SDL_FULLSCREEN;
+	ecran = SDL_SetVideoMode(m_config.size.w, m_config.size.h, 32, flags);
+
+	if(ecran == NULL)
+		throw std::string("Erreur à l'ouverture de la fenêtre.");
+}
+
+void Printer::loadErr()
+{
+	m_err = IMG_Load("err.png");
+	if(m_err == NULL)
+		throw std::string("Erreur au chargement de l'image d'erreur.");
+}
+
+void Printer::loadDiap()
+{
+	m_diapTimer = new Timer(m_config.time,
+			boost::bind(&Printer::next, this, false) );
+}
+
+void Printer::loadFirst()
+{
+	m_act = 0;
+
+	if( !m_config.prep )
+		prepare( &m_picts[m_act], true );
+
+	if( !m_config.begin )
+		load( &m_picts[m_act] );
+
+	if( m_config.text && !m_config.ptext )
+		loadTxt( &m_picts[m_act], m_config.textaa, m_act + 1 );
+
+	if( m_config.diap )
+		m_diapTimer->reset();
+
+	std::string caption("sdlvis - ");
+	caption += m_picts[m_act].path.filename().string();
+	SDL_WM_SetCaption(caption.c_str(), NULL);
+}
+
+void Printer::loadKeys(sdl::Event* event)
+{
+	event->addQuitKey(SDLK_q);
+	event->addQuitKey(SDLK_ESCAPE);
+
+	m_keys.resize(NUMBER);
+
+	m_keys[NEXT].addKey(SDLK_RIGHT);
+	event->addPEvent( "next", &m_keys[NEXT],
+			boost::bind(&Printer::next, this, false) );
+
+	m_keys[PREV].addKey(SDLK_LEFT);
+	event->addPEvent( "prev", &m_keys[PREV],
+			boost::bind(&Printer::prev, this, false) );
+
+	m_keys[FIRST].addKey(SDLK_UP);
+	event->addPEvent( "first", &m_keys[FIRST],
+			boost::bind(&Printer::first, this) );
+
+	m_keys[LAST].addKey(SDLK_DOWN);
+	event->addPEvent( "last", &m_keys[LAST],
+			boost::bind(&Printer::last, this) );
+}
+
+
---
+++ print.hpp
@@ -0,0 +1,80 @@
+#ifndef DEF_PRINT
+#define DEF_PRINT
+
+#include <boost/bind.hpp>
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDLP_event.hpp>
+#include <SDLP_tools.hpp>
+
+#include "config.hpp"
+#include "timer.hpp"
+
+class Printer
+{
+	public:
+		Printer(config conf);
+		void run();
+		~Printer();
+
+	private:
+		struct Picture
+		{
+			Picture() : path(""), surf(NULL), txt(NULL), redx(1.0), redy(1.0), prepared(false)
+			{}
+
+			path_t path;
+			SDL_Surface* surf;
+			SDL_Surface* txt;
+			double redx;
+			double redy;
+
+			bool bigger; // Plus grand que la fenêtre
+			bool prepared; // Évite de préparer 2 foix
+			bool aa;
+		};
+
+		config m_config;
+		std::vector<Picture> m_picts;
+
+		void load();
+		bool prepare(Picture* pict, bool force_quard = false);
+		void load(Picture* pict, bool aa = true); // La picture doit être préparée avant
+		void loadTxt(Picture* pict, bool aa = false, size_t number=0);
+		void loadFont();
+		void loadWindow(); // Chargement de la fenêtre
+		void loadErr(); // Chargement de l'image d'erreur
+		void loadFirst();
+		void loadDiap();
+		void loadKeys(sdl::Event* event);
+		void toggleAA();
+		bool m_aa;
+
+		inline bool mute();
+		inline bool verbose();
+		inline bool highverb();
+
+		Timer* m_diapTimer; // Le timer pour le diaporama
+
+		SDL_Surface* ecran;
+		SDL_Surface* m_err; // Image à afficher en cas d'erreur
+		TTF_Font* m_font;
+
+		enum{NEXT, PREV, FIRST, LAST, TOGGLE, NUMBER};
+		std::vector< sdl::AnEvent > m_keys;
+
+		size_t m_act;
+		void next(bool ret = false); // fonctions de déplacement
+		void prev(bool ret = false);
+		void first();
+		void last();
+		void update(size_t last);
+
+		Printer();
+		Printer(const Printer&);
+		Printer& operator=(const Printer&);
+};
+
+#endif
