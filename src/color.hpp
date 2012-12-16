da8558d3c82bea2849df06d7e20e24e82e150550
lucas8 <luc.linux@mailoo.org>
2012-12-16 21:28:59 +0100

    Ajout des fichiers pour la gestion de couleur.

---
+++ src/color.hpp
@@ -0,0 +1,46 @@
+
+/*
+ * Copyright (C) 2012 Chabassier Luc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef DEF_COLOR
+#define DEf_COLOR
+
+#include <SDL/SDL.h>
+#include <string>
+
+class Color
+{
+	public:
+		Color();
+		color(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+		Color(const Color& cp);
+		Color(const std::string& src); // Hexadécimal, exemple : "0xFF0000" ou "FF0000"
+		Color(const SDL_Color& col);
+		Color(Uint32 col);
+
+		Color& operator=(const Color& cp);
+
+		Uint32 get32() const;
+		SDL_Color getSdl() const;
+		std::string getHex() const;
+
+		// TODO
+	private:
+		Uint8 r,g,b,a;
+};
+
